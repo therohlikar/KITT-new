11474bb6a6cbb7a484fc6f8fcfd59be8a982dc4a
RJ <radek.jenik36@gmail.com>
2023-03-03 09:04:33 +0100

    Initial Commit

---
+++ KITT-testApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  KITT-testApp
+//
+//  Created by Radek Jeník on 3/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KITT-testApp/KITT_testAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KITT_testAppApp.swift
+//  KITT-testApp
+//
+//  Created by Radek Jeník on 3/3/23.
+//
+
+import SwiftUI
+
+@main
+struct KITT_testAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
