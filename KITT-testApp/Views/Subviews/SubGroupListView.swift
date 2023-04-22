478ba439d5a61100a9b747d2d733c49a3617c878
RJ <radek.jenik36@gmail.com>
2023-04-22 21:00:51 +0200

    scroll to

--- KITT-testApp/Views/Subviews/SubGroupListView.swift
+++ KITT-testApp/Views/Subviews/SubGroupListView.swift
@@ -15,41 +15,66 @@ struct SubGroupListView: View {
     var favoritesOnly: Bool
     
     var body: some View {
-        ScrollView {
-            ForEach(dict, id: \.description) { subgroup in
-                LazyVStack {
+        ScrollViewReader { value in
+            ScrollView {
+                GroupBox(content: {
+                    VStack(alignment: .leading){
+                        ForEach(dict, id: \.description) { subgroup in
+                            HStack{
+                                Button("- \(subgroup.description.uppercased())") {
+                                    withAnimation(.spring()) {
+                                        value.scrollTo(subgroup.description, anchor: .top)
+                                    }
+                                }
+                                .font(.callout)
+                                .foregroundColor(.secondary)
+                                
+                                Spacer()
+                            }
+                            .padding(.bottom, 2)
+                        }
+                    }
+                }, label: {
                     HStack{
-                        Text(subgroup.description.uppercased())
+                        Text("KATEGORIE")
                             .font(.caption)
                         Spacer()
                     }
-                    
-                    ForEach(currentGroup.ciArray) { item in
-                        if item.wrappedSubgroup == subgroup.description {
-                            if favoritesOnly == false || item.favorited == favoritesOnly {
-                                NavigationLink {
-                                    ContentItemView(item: item)
-                                } label: {
-                                    ContentItemRowView(item: item)
+                })
+                ForEach(dict, id: \.description) { subgroup in
+                    LazyVStack {
+                        HStack{
+                            Text(subgroup.description.uppercased())
+                                .font(.caption)
+                            Spacer()
+                        }
+                        
+                        ForEach(currentGroup.ciArray) { item in
+                            if item.wrappedSubgroup == subgroup.description {
+                                if favoritesOnly == false || item.favorited == favoritesOnly {
+                                    NavigationLink {
+                                        ContentItemView(item: item)
+                                    } label: {
+                                        ContentItemRowView(item: item)
+                                    }
+                                    .isDetailLink(false)
                                 }
-                                .isDetailLink(false)
                             }
                         }
                     }
                 }
             }
-        }
-        .padding()
-        .navigationBarTitleDisplayMode(.inline)
-        .navigationTitle(currentGroup.wrappedTitle)
-        .onAppear{
-            for item in currentGroup.ciArray {
-                if !item.wrappedSubgroup.isEmpty && !dict.contains(where: {$0 == item.wrappedSubgroup}) {
-                    dict.append(item.wrappedSubgroup)
+            .padding()
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle(currentGroup.wrappedTitle)
+            .onAppear{
+                for item in currentGroup.ciArray {
+                    if !item.wrappedSubgroup.isEmpty && !dict.contains(where: {$0 == item.wrappedSubgroup}) {
+                        dict.append(item.wrappedSubgroup)
+                    }
                 }
             }
         }
+        
     }
-    
-    
 }
