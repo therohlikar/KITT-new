55b329757e59553ab073e6e9f2f8a8d9c32ad7ec
RJ <radek.jenik36@gmail.com>
2023-07-22 18:09:49 +0200

    news allRead

--- KITT-testApp/Views/Subviews/NewsView.swift
+++ KITT-testApp/Views/Subviews/NewsView.swift
@@ -12,29 +12,46 @@ struct NewsView: View {
     @FetchRequest(sortDescriptors: [NSSortDescriptor(key: "version", ascending: false)]) var news: FetchedResults<Version>
     
     var body: some View {
-        List(news, id:\.self){ version in
-            Section {
-                Text(version.wrappedContent)
-                    .swipeActions {
-                        Button {
-                            version.read = true
-                        } label: {
-                            Label("", systemImage: "envelope.open.fill")
-                                .labelsHidden()
+        VStack{
+            Button {
+                self.readAll()
+            } label: {
+                Text("Označit vše za přečtené")
+            }
+
+            
+            List(news, id:\.self){ version in
+                Section {
+                    Text(version.wrappedContent)
+                        .swipeActions {
+                            Button {
+                                version.read = true
+                            } label: {
+                                Label("", systemImage: "envelope.open.fill")
+                                    .labelsHidden()
+                            }
+                            .tint(.blue)
                         }
-                        .tint(.blue)
+                } header: {
+                    HStack{
+                        Image(systemName: version.read ? "envelope.open.fill" : "envelope.fill")
+                            .foregroundColor(.blue)
+                        Text(version.wrappedVersion)
+                            .font(.headline)
                     }
-            } header: {
-                HStack{
-                    Image(systemName: version.read ? "envelope.open.fill" : "envelope.fill")
-                        .foregroundColor(.blue)
-                    Text(version.wrappedVersion)
-                        .font(.headline)
                 }
             }
         }
         .onDisappear{
             dc.save()
+        }
+    }
+    
+    func readAll() {
+        for item in news {
+            if !item.read {
+                item.read = true
+            }
         }
     }
 }
